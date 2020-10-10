15e55b975fba215dc8ac45200f66e53543815a78
Iapetus-11 <olimdahacker@gmail.com>
2020-10-10 13:56:01 -0400

    make pal_from_image a classmethod

--- palette_gen.py
+++ palette_gen.py
@@ -4,47 +4,6 @@ import json
 import cv2
 import os
 
-def pal_from_image(image_file, dest_dims, verbose):
-    if verbose:
-        print(f'Processing: {image_file}')
-
-    img = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
-
-    if img is None:
-        return
-
-    if img.shape[1] != dest_dims[1] or img.shape[0] != dest_dims[0]:
-        img = cv2.resize(img, desired_dims)
-
-    p_count = 0
-    avgs = [0, 0, 0]  # bgr
-
-    for row in img:
-        for pixel in row:
-            if len(pixel) > 3 and pixel[3] < 255:
-                return
-
-            avgs[0] += pixel[0]
-            avgs[1] += pixel[1]
-            avgs[2] += pixel[2]
-
-            p_count += 1
-
-    avgs[0] /= p_count
-    avgs[1] /= p_count
-    avgs[2] /= p_count
-
-    avgs.reverse()  # turn into rgb
-
-    b = base64.b64encode(cv2.imencode('.png', img)[1]).decode('utf-8')
-
-    return (
-        [[int(avg/128) for avg in avgs], image_file],
-        [[int(avg/64) for avg in avgs], image_file],
-        [[int(avg/32) for avg in avgs], image_file],
-        {image_file: b}
-    )
-
 class Palette:
     def __init__(self, *, resolution: int = 16, source_dir: str = '.', verbose: bool = False):
         self.source_dir = source_dir
@@ -58,7 +17,7 @@ class Palette:
         image_files = (*filter((lambda file: (file.endswith('.png') or file.endswith('.jpg'))), next(os.walk(self.source_dir))[2]),)
 
         with Pool(8) as pool:
-            raw_p = (*filter((lambda e: bool(e)), pool.map(pal_from_image, image_files)),)
+            raw_p = (*filter((lambda e: bool(e)), pool.map(self.pal_from_image, image_files),)
 
         map_bi = []
         map_quad = []
@@ -80,3 +39,43 @@ class Palette:
         }
 
         if self.verbose: print('Done!')
+
+    def pal_from_image(image_file):
+        if self.verbose: print(f'Processing: {image_file}')
+
+        img = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
+
+        if img is None:
+            return
+
+        if img.shape[1] != self.dest_dims[1] or img.shape[0] != self.dest_dims[0]:
+            img = cv2.resize(img, self.dest_dims)
+
+        p_count = 0
+        avgs = [0, 0, 0]  # bgr
+
+        for row in img:
+            for pixel in row:
+                if len(pixel) > 3 and pixel[3] < 255:
+                    return
+
+                avgs[0] += pixel[0]
+                avgs[1] += pixel[1]
+                avgs[2] += pixel[2]
+
+                p_count += 1
+
+        avgs[0] /= p_count
+        avgs[1] /= p_count
+        avgs[2] /= p_count
+
+        avgs.reverse()  # turn into rgb
+
+        b = base64.b64encode(cv2.imencode('.png', img)[1]).decode('utf-8')
+
+        return (
+            [[int(avg/128) for avg in avgs], image_file],
+            [[int(avg/64) for avg in avgs], image_file],
+            [[int(avg/32) for avg in avgs], image_file],
+            {image_file: b}
+        )
